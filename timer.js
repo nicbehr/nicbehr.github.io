a543b9de91df7981ee1eacc8cf08244d4c63c086
nicbehr <56953150+nicbehr@users.noreply.github.com>
2021-05-13 16:40:48 +0200

    Add files via upload

--- timer.js
+++ timer.js
@@ -17,8 +17,8 @@ var x = setInterval(function () {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+    document.getElementById("countdown").innerHTML = days + " Tage " + hours + " Stunden <br>"
+        + minutes + " Minuten " + seconds + " Sekunden ";
 
     // If the count down is finished, write some text
     if (distance < 0) {
